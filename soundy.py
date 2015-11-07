4f029f54427a8f0aa02e1ed1580352aa35acc6ad
Hastings Greer <hastings.greer@gmail.com>
2015-11-07 18:22:22 -0500

    magic

---
+++ soundy.py
@@ -0,0 +1,26 @@
+import pygame
+pygame.mixer.init()
+import time
+
+sound = pygame.mixer.Sound("sumsad.ogg")
+
+rate = 22050
+
+arr = pygame.sndarray.array(sound)
+idx = 240000
+
+timestep = .1
+
+steps = rate * timestep
+
+elapsed = 0
+while True:
+    t = time.time()
+    print elapsed
+    sound = pygame.sndarray.make_sound(arr[idx:idx + steps])
+    elapsed = time.time() - t
+    time.sleep(timestep - elapsed)
+    
+    sound.play()
+    
+    idx += steps
