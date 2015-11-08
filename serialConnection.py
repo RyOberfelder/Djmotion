3e9a26d0a55cacb1ef60dba9775295b2f9eefadc
Ryan Oberfelder <ryanoberfelder@Ryans-Air.wireless.duke.local>
2015-11-07 22:21:37 -0500

    here are Ryan Parts

--- serialConnection.py
+++ serialConnection.py
@@ -1,6 +1,8 @@
 import serial
+import time	
+from threading import Thread
 #ttyACM0 is the name of the arduino
-ser=serial.Serial(port='/dev/ttyACM1', baudrate=9600)
+ser=serial.Serial(port='/dev/cu.usbmodem1421', baudrate=9600)
 while True:
     time.sleep(.01)
     out = ''
