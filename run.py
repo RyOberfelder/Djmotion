43a61ba6899d7779f09ef6e90222a1019eaa4905
Ryan Oberfelder <ryanoberfelder@Ryans-Air.wireless.duke.local>
2015-11-08 09:00:24 -0500

    Here is the run updated

--- run.py
+++ run.py
@@ -1,8 +1,17 @@
 import soundy
+print 'imported soundy'
 import sys
-sys.path += ['C:/Users/Deven/Documents/Workspace/Djmotion/LeapSDK/lib',
-             'C:/Users/Deven/Documents/Workspace/Djmotion/LeapSDK/lib/x86']
+print 'imported sys'
+sys.path += ['/Users/ryanoberfelder/Programming/Projects/Djmotion/LeapSDK/lib',]
+print 'sys path added'
 import Leap
+print 'imported leap'
+import os
+print 'imported os'
+import serial
+print 'imported serial super cereal'
+import time
+print 'imported time'
 
 class SimpleListener(Leap.Listener):
 
@@ -136,16 +145,109 @@ class SimpleListener(Leap.Listener):
         self.distortTimer = self.MAX_INTERVAL
         self.previousFrame = {'gesture': 'circle', 'frame': gesture.frame}
 
+
+tokenlength = 31
+pastState = None
+def child(pipeout):
+  ser=serial.Serial(port='/dev/cu.usbmodem1421', baudrate=9600)
+  bottles = 99
+  count = 0
+  out = ''
+  while True:
+      if ser.inWaiting() > 0:
+          out += ser.read(1)
+          count = count + 1
+      if out != '' and count >=tokenlength: #and out[0]=='A':
+          os.write(pipeout, out)
+          count = 0
+          out = ''
+
+def parent():
+    pipein, pipeout = os.pipe()
+    if os.fork() == 0:
+        child(pipeout)
+    else:
+        counter = 1
+        while True:
+            verse = os.read(pipein, 117)
+            print verse
+            keyvalPairs = toKeyPair(verse)
+            print keyvalPairs
+            # compare presentState and Past state
+
+            stateChange(keyvalPairs)
+
+            # update past state
+            pastState = keyvalPairs
+
+def toKeyPair(rawInput):
+    rawInput = sorted(rawInput.split(','))[1:]
+    keyPairing = {}
+    for Input in rawInput:
+        keyPairing[str(Input[0])] = toDec(Input[1:])
+    return keyPairing
+
+
+
+def toDec(string):
+    def getEquivlent(char):
+        if char == 'a':
+            return 10
+        elif char == 'b':
+            return 11
+        elif char == 'c':
+            return 12
+        elif char =='d':
+            return 13
+        elif char =='e':
+            return 14
+        elif char =='f':
+            return 15
+        elif char <= '9' or char >= '0':
+            return int(char)
+        else:
+            raise Error('WTF Mate?')
+    if len(string) > 1:
+        return getEquivlent(string[0])*16 + getEquivlent(string[1])
+    else:
+        return int(string)
+
+def stateChange(keyValues):
+    if(pastState==None):
+        return
+    if(keyValues['A'] != pastState['A'] ):
+        soundy.robot()
+    if(keyValues['B'] != pastState['B'] ):
+        pass
+        #doB()
+    if(keyValues['C'] != pastState['C'] ):
+        pass
+        #doC()
+    if(keyValues['D'] != pastState['D'] ):
+        pass
+        #doD()
+    if(keyValues['E'] != pastState['E'] ):
+        pass
+        #doE()
+    if(keyValues['W'] != pastState['W'] ):
+        pass
+        #doW()
+    if(keyValues['X'] != pastState['X'] ):
+        pass
+        #doX()
+    if(keyValues['Y'] != pastState['Y'] ):
+        pass
+        #doY()
+    if(keyValues['Z'] != pastState['Z'] ):
+        pass
+        #doZ()
+
+
 if __name__ == '__main__':
     listener = SimpleListener()
     controller = Leap.Controller()
 
     controller.add_listener(listener)
     # Keep Process Running until it fails
-    print u'Press Enter to quit...'
-    try:
-        sys.stdin.readline()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        controller.remove_listener(listener)
+
+    parent()
--- serialConnection.py
+++ serialConnection.py
@@ -1,5 +1,5 @@
 import serial
-import time	
+import time
 from threading import Thread
 #ttyACM0 is the name of the arduino
 ser=serial.Serial(port='/dev/cu.usbmodem1421', baudrate=9600)
@@ -10,6 +10,17 @@ while True:
         out += ser.read(1)
     if out != '':
         print out
-    
 
 
+import threading
+
+def worker():
+    """thread worker function"""
+    print 'Worker'
+    return
+
+threads = []
+for i in range(5):
+    t = threading.Thread(target=worker)
+    threads.append(t)
+    t.start()
