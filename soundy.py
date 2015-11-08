bc380892c4022f4b3c20377702038e9c8fc5df98
Hastings Greer <hastings.greer@gmail.com>
2015-11-07 21:00:05 -0500

    woo

--- soundy.py
+++ soundy.py
@@ -7,7 +7,9 @@ import matplotlib.pyplot as plt
 
 import numpy.fft as f
 
-sound = pygame.mixer.Sound("Music/Happy Jinjo House - Banjo-Tooie.ogg")
+#sound = pygame.mixer.Sound("Music/I would walk 500 miles - The Proclaimers.ogg")
+#sound = pygame.mixer.Sound("Music/Happy Jinjo House - Banjo-Tooie.ogg")
+sound = pygame.mixer.Sound("sumsad.ogg")
 
 rate = 22050
 
@@ -22,7 +24,21 @@ elapsed = 0
 
 limit = np.max(a)
 
+ahat = f.rfft(a, axis = 0)
+w = f.rfftfreq(a.shape[0], )
+
+high_t = ahat.copy()
+high_t[w < .02] = 0
+high = f.irfft(high_t, axis = 0)
+
+low_t = ahat.copy()
+low_t[w > .02] = 0
+low = f.irfft(low_t, axis = 0)
+
 sound.play()
+
+time = np.array((np.linspace(0, 300, len(a)), np.linspace(0, 300, len(a)))).transpose()
+
 while False:
     t = time.time()
     print elapsed
