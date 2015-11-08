be0f411d65f47e086187093ce8d6c85ee4c63d1b
Hastings Greer <hastings.greer@gmail.com>
2015-11-08 01:47:31 -0500

    fix shit

--- soundy.py
+++ soundy.py
@@ -5,44 +5,79 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
+
+
+
+
+
+
+
+
+
+
 import numpy.fft as f
 
-#sound = pygame.mixer.Sound("Music/I would walk 500 miles - The Proclaimers.ogg")
-#sound = pygame.mixer.Sound("Music/Happy Jinjo House - Banjo-Tooie.ogg")
-#sound = pygame.mixer.Sound("sumsad.ogg")
-sound = pygame.mixer.Sound("Music/CrazyFrog-AxelF.ogg")
+#sound = pygame.mixer.Sound("Music/Iwouldwalk500miles-TheProclaimers.ogg")
+#sound = pygame.mixer.Sound("Music/HappyJinjoHouse-Banjo-Tooie.ogg")
+sound = pygame.mixer.Sound("Music/sumsad.ogg")
+#sound = pygame.mixer.Sound("Music/CrazyFrog-AxelF.ogg")
+#sound = pygame.mixer.Sound("Music/WagonWheelDariusRucker.ogg")
 
-rate = 22050
+
 
 arr = pygame.sndarray.samples(sound)
-idx = 240000
-a = arr.copy()
-timestep = .1
 
-steps = rate * timestep
+a = arr.copy()
+
+
+
+timestep = 2
+rate = 22050
+
+beat = 60./122 * rate
+
+steps = 2*int(rate * timestep / 2)
+
+windows = []
 
 elapsed = 0
+idx = 0
+while elapsed < sound.get_length():
+    windows += [idx]
+    elapsed += timestep
+    idx += steps
+
+windows = windows[:-1]
 
 limit = np.max(a)
+high = a * 0
+med = a * 0
+low = a * 0
+for p in windows:
+    ahat = f.rfft(a[p:p + steps], axis = 0)
+    print ahat.shape
+    w = f.rfftfreq(steps, 1./rate)
 
-ahat = f.rfft(a, axis = 0)
-w = f.rfftfreq(a.shape[0], sound.get_length / a.shape[0])
+    high_t = ahat.copy()
+    high_t[w < 1400] = 0
+    high [p:p + steps]= f.irfft(high_t, axis = 0)
 
-high_t = ahat.copy()
-high_t[w < .06] = 0
-high = f.irfft(high_t, axis = 0)
-
-low_t = ahat.copy()
-low_t[w > .02] = 0
-low = f.irfft(low_t, axis = 0)
+    low_t = ahat.copy()
+    low_t[w > 600] = 0
+    low[p:p + steps] = f.irfft(low_t, axis = 0)
 
 med = a - high - low
 
+
+scratch = pygame.sndarray.make_sound(pygame.sndarray.array(pygame.mixer.Sound("Music/scratch.ogg"))[.5 * rate:.7 * rate])
+
+
 sound.play()
 
-ind= np.arange(len(w)/2)
+ind= np.arange(a.shape[0]/2)
 
-tt = np.linspace(0, sound.get_length(), len(a
+tt = np.linspace(0, sound.get_length(), len(a))
 time = np.array((tt, tt)).transpose()
 
 while False:
@@ -87,4 +122,5 @@ def nofilt():
 def robot():
     filt = lambda x: x * np.sin(time * 200)
     
-  
+def scratchback(n):
+    scratch.play();arr[beat*8 :] = arr[:-beat*8 ]
